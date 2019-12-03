60d0d367662f9096cdf509dac0882a0e20ac006d
Vladimir Zoubritsky <vladimir@reshuffle.com>
2019-12-03 22:30:33 +0200

    Explicitly enable GraphQL playground in production

--- backend/_handler.js
+++ backend/_handler.js
@@ -6,7 +6,14 @@ import resolvers from './resolvers';
 
 const app = express();
 
-const apollo = new ApolloServer({ typeDefs, resolvers });
+const apollo = new ApolloServer({
+  typeDefs,
+  resolvers,
+  /* production note: remove the two following lines to enable the playground
+   * only in development mode */
+  introspection: true,
+  playground: true,
+});
 apollo.applyMiddleware({ app });
 app.use(defaultHandler);
 
